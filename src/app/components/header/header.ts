0255f401f5654c6d2b9bb527848d774512be6d0b
Pierre <pierre.chevallier@hardis-group.com>
2026-07-30 17:00:17 +0200

    Make the header sticky and add a background blur on it when scrolling

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID, Signal, signal } from '@angular/core';
 import { HeaderNavLinks } from './header-nav-links/header-nav-links';
-import { NgOptimizedImage } from '@angular/common';
+import { isPlatformBrowser, NgOptimizedImage } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { fromEvent, map } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'header-menu',
@@ -8,4 +11,19 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  private readonly platformId: Object = inject(PLATFORM_ID);
+  private readonly isBrowser = isPlatformBrowser(this.platformId);
+  private readonly scrollThreshold = this.isBrowser
+    ? 10 * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    : 160; // fallback for SSR
+  protected readonly isScrolledAboveThreshold: Signal<boolean> = this.isBrowser
+    ? toSignal(
+        fromEvent(window, 'scroll').pipe(
+          map(() => window.scrollY > this.scrollThreshold),
+          startWith(window.scrollY > this.scrollThreshold),
+        ),
+        { initialValue: false },
+      )
+    : signal(false);
+}
