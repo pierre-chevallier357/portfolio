dcb34215493562797fb43526b4b24c39f3faf4c2
Pierre <pierre.chevallier@hardis-group.com>
2026-08-31 11:46:44 +0200

    Make all sections consistent in width, margins and gap

--- src/app/shared/background/aura-background.ts
+++ src/app/shared/background/aura-background.ts
@@ -14,6 +14,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { AuraDot } from './aura-dot';
 import { ThemeStore } from '../../core/theme/theme-store';
 
+// TODO check for color accessibility with this background color (mix of all dots colors) #1F1E1D
 const DOTS_COLORS: string[] = ['#ff5e00', '#f2ff00', '#00e5ff', '#15ff00', '#ff00f7'];
 const BACKGROUND_LIGHT_COLOR: string = '#ede7de';
 const BACKGROUND_DARK_COLOR: string = '#025a4e';
